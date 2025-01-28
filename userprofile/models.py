a11fb1c6869acabb4b9554f82c8fde3b56286766
Kirlian Verus <127189354+Kyverus@users.noreply.github.com>
2025-01-29 00:56:41 +0800

    User Profile Implementation
    Added Profile Model
    Added profile layout, index and update templates
    Added forms for updating profile
    Added showing and updating views
    Redesigned user login and register templates

---
+++ userprofile/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(User, auto_now=True)
+    avatar = models.ImageField(default='avatars/default_avatar.png', blank=True, upload_to="avatars")
+    public_name = models.SlugField(max_length=60, blank=True)
+    private = models.BooleanField(default=True)
+    contact_number = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
+# Create User Profile by default when user signs up
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance, public_name=instance.username.lower())
+        user_profile.save()
+
+# Automate Profile Creation
+post_save.connect(create_profile, sender=User)
